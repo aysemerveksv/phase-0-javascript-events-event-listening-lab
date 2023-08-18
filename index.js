6f507556d61c20e2fa9e51383288efcbe850682e
Ayşe Merve Kosova <mervekosova1@hotmail.com>
2023-08-18 17:46:57 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 function addingEventListener() {
-}
+    // Get the input element with the id 'button'
+    const button = document.getElementById('button');
+  
+    // Add an event listener for the 'click' event
+    button.addEventListener('click', function() {
+      alert('Button clicked!');
+    });
+  }
+  
+  // Call the function to set up the event listener
+  addingEventListener();
